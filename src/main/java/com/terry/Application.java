81fb8f9b347f2e963f379e180f4075b36ef2c975
Terry <jie.shi@hp.com>
2015-08-19 14:37:28 +0800

    clean un-used code.

--- src/main/java/com/terry/Application.java
+++ src/main/java/com/terry/Application.java
@@ -1,45 +1,21 @@
 package com.terry;
 
-import java.util.Date;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * http://kielczewski.eu/2014/04/spring-boot-mvc-application/
  *
  */
-//@RestController
-@Controller
 @EnableAutoConfiguration
-//@SpringBootApplication
+//@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 @ComponentScan
 public class Application {
 
-    private String message = "Hello World!!!";
-    
-//    @RequestMapping("/index")
-//    String home() {
-//        return "index";
-//    }
-//
-//    @RequestMapping("/welcome")
-//    public String welcome(Map<String, Object> model) {
-//        model.put("time", new Date());
-//        model.put("message", this.message);
-//        return "welcome";
-//    }
-    
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
-        
 //        SpringApplication app = new SpringApplication(Application.class);
 //        app.addListeners(…); // such as ApplicationStartedEvent
 //        app.setShowBanner(false);
